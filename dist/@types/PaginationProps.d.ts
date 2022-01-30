c038f3ce1fe11112e5cc812e60a7e5c5a4fa4448
jose alejandro godoy <desarrollo1@estacionmaritima.com>
2022-01-30 01:04:06 +0100

    scss para la tabla

---
+++ dist/@types/PaginationProps.d.ts
@@ -0,0 +1,5 @@
+export interface PaginationProps {
+    per_page?: number | string;
+    total?: number;
+    page?: number;
+}
---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+export { default as DataTable } from "@/lib/DataTable.vue";
+export { default as TableHead } from "@/lib/Components/Table/TableHeadCell.vue";
+export { default as TableHeadCell } from "@/lib/Components/Table/TableHeadCell.vue";
+export { default as TableBody } from "@/lib/Components/Table/TableBodyCell.vue";
+export { default as TableBodyCell } from "@/lib/Components/Table/TableBodyCell.vue";
+export { default as Pagination } from "@/lib/Components/Pagination/Pagination.vue";
+export { default as PaginationLink } from "@/lib/Components/Pagination/PaginationLink.vue";
